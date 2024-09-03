1a0ae5ee88a06948099e64e9e968eeff2236324d
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-09-03 23:34:08 +0530

    2nd change

--- main.cpp/main.cpp
+++ main.cpp/main.cpp
@@ -4,5 +4,13 @@ int main(){
     int n, k, count = 0;
     cout << "Enter the number of integers: ";
     cin >> n;
+
+    cout << "Enter the integers: ";
+    for(int i = 0; i < n; i++){
+        cin >> k;
+        if(k % 3 == 0 && k % 5 == 0 && k % 7 == 0){
+            count++;
+        }
+    }
 return 0;
-}
+}   // end of
