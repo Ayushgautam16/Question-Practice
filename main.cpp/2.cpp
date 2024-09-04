418da4547cdae51a5be955dc0de22b5b50da27ef
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-09-04 17:14:45 +0530

    1

--- main.cpp/2.cpp
+++ main.cpp/2.cpp
@@ -232,3 +232,28 @@
 // }
 
 
+// for finding the gcd number
+
+#include<iostream>
+using namespace std;
+int main(){
+int m, n;
+
+cout << "Enter two positive integers: ";
+cin >> m >> n;
+while (m!=n){
+{
+   
+    if (m > n)
+    
+        m = m - n;
+    
+    else if (n > m)
+    
+        n = n - m;
+    
+}
+cout<<" num is the gcd"<<m<<endl;
+return 0;
+}
+}
