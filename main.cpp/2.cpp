0e60a41933270239096a2eb5067c76a28921f475
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-09-05 22:45:11 +0530

    1

--- main.cpp/2.cpp
+++ main.cpp/2.cpp
@@ -16,6 +16,26 @@
 // return 0;
 // }
 
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int i, n, sum=0;
+
+// cout << "Enter a positive integer: ";
+// cin >> n;
+// for (int i = 0; i < n; i++)
+// {
+//     sum = sum +i;
+// }
+
+// cout << "Sum of natural numbers from 1 to " << n << " is: " << sum << endl;
+// cin >> n; // To take input from user
+
+// return 0;
+// }
+
 // prog for finding the factorial of the number
 
 // #include<iostream>
