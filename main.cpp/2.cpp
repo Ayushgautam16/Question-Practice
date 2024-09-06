1ad755c97cd7b953efff1d625362fc0f3725ed80
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-09-06 21:47:56 +0530

    2

--- main.cpp/2.cpp
+++ main.cpp/2.cpp
@@ -17,6 +17,24 @@
 // }
 
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int i, n, sum=0;
+
+// cout << "Enter a positive integer: ";
+// cin >> n;
+// for (int i = 0; i < n; i++)
+// {
+//     sum = sum +i;
+// }
+
+// cout << "Sum of natural numbers from 1 to " << n << " is: " << sum << endl;
+// cin >> n; // To take input from user
+
+// return 0;
+// }
+
 
 // #include<iostream>
 // using namespace std;
