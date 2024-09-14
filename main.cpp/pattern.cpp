96ff879f3d36006770ef4c41ba5d9acc1fc9b69d
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-09-14 22:42:57 +0530

    2

--- main.cpp/pattern.cpp
+++ main.cpp/pattern.cpp
@@ -1,6 +1,29 @@
 #include<iostream>
 using namespace std;
 int main(){
+int i,n;
+cout << "numobjects" << endl;
+cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j<n; j++)
+        {
+            
+            if (i>j)
+            {
+                /* code */
+                cout << "* ";
+            }
+            cout <<endl;
+
+            
+        }
+    }
+    
+
+
+
+
 
 return 0;
 }
