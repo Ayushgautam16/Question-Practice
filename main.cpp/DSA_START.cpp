e9c6d632d0db6eabf1f5d01569512f23af674182
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-09-17 16:26:16 +0530

    1

---
+++ main.cpp/DSA_START.cpp
@@ -0,0 +1,50 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+// // int A[7] ={1,2,3,4,56,7,2};
+
+//     // cout <<sizeof(A)<<endl;
+//     // cout<<sizeof A[3];
+
+//     // int a[10]= {1,2,3,4,57,8};
+//     // {
+//     //     for (int i = 0; i <= 10; i++)
+//     //     {
+//     //         /* code */
+//     //         cout<<a[i]<<endl;
+//     //     }
+
+
+//     int n;
+//     cout<<"enter the value ";
+//     cin>>n;
+//     int A[n];
+//     A[0]=2;
+
+
+//     for(int x:A)
+//     {
+//         cout<<x<<endl;
+//     }
+        
+
+
+// }
+
+
+// Arrays struct
+
+
+
+
+
+    
+
+
+
+
+    
+
+
+
---
+++ main.cpp/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+  // int A[7] ={1,2,3,4,56,7,2};
+
+    // cout <<sizeof(A)<<endl;
+    // cout<<sizeof A[3];
