97f67cbe4e868c25b27313fe118648a7327a6431
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-10-11 10:09:00 +0530

    2

---
+++ main.cpp/start.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int i[5] = {0,1,2,3,4};
+
+for(int j = 0; j < 5; j++)
+    cout << i[j] << " ";
+cout <<sizeof(i)<<endl;
+return 0;
+
+}   // end of main function.
+
+// for each loop 
+
