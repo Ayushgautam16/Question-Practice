164ad0294753b5e22d10bf98f5611b077aae08dd
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-02-21 23:00:35 +0530

    1

---
+++ main.cpp/DSA Phase2/1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int number ;
+    cin >> number;
+    printf("your id is %d\n", number);
+}
---
+++ main.cpp/DSA Phase2/basics.cpp
@@ -0,0 +1,144 @@
+// #include <iostream>
+
+// now for including all the libraries 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // cout<<"hey ayush"<<endl;
+    // cout<<"hey \n ayush \n";
+
+    // // \n is used for the new line print
+    // // either you can use the endl
+
+    // int x;
+    // cin>>x;
+    // cout <<"value of x: "<<x<<endl;
+
+    // int a,b;
+    // cin>>a>>b;
+    // cout<<"sum of a and b: "<<a+b<<endl;
+
+
+
+    // data types?
+
+// int?
+// int ayush = 100;
+// return 0;
+ 
+// // long
+// long x = 12;
+// cin>>x;
+
+// long long z = 1000000000;
+
+// float
+
+// float a;
+// cin >> a;
+// cout<<"value of a: "<<a<<endl;
+
+// double a;
+// cin >> a;
+// cout<<"value of a: "<<a<<endl;
+
+
+// string str = "hey how you are feeling now :";
+// cout<<str<<endl;
+
+
+// string s;
+// // cout<<"this is the text " << s <<endl;
+
+// getline(cin, s);
+// cout << "this is the text " << s <<endl;
+
+
+// char
+
+// char ch ;
+// cin >>ch;
+// cout <<ch;
+
+// char charr[10]; 
+
+// int age;
+// cin >> age;
+// if (age>=18) {
+//     cout << "You are eligible to vote";
+
+// }
+// else if (age<=18) {
+//     cout << "You are not eligible to vote";
+// }
+// return 0;
+
+// int marks;
+
+// cout << "Enter your marks: ";
+// cin >> marks;
+
+// if (marks >= 90) {
+//     cout << "Grade: A";
+// }
+// else if (marks >=70){
+//     cout << "Grade: B";
+// }
+// else if (marks >=50){
+//     cout << "Grade: C";
+// }
+// else if (marks >=33){
+//     cout << "Grade: D";
+// }
+// else if (marks < 33){
+//     cout << "Grade: F";
+// }
+
+// && this means and in the syntax
+// || this means or in the syntax
+
+
+// now giving the exa,ple of the 
+
+// int age ;
+// cin >> age ;
+// if (age <65){
+//     cout << "You are not eligible to vote";
+//     if (age==60){
+//         cout << " You are eligible for early voting";
+//     }
+//     // } this is called the nested loop?
+// }
+
+int day;
+cin >> day;
+
+switch (day) {
+    case 1:
+        cout << "Monday";
+        break;
+    case 2:
+        cout << "Tuesday";
+        break;
+    case 3:
+        cout << "Wednesday";
+        break;
+    case 4:
+        cout << "Thursday";
+        break;
+    case 5:
+        cout << "Friday";
+        break;
+    case 6:
+        cout << "Saturday";
+        break;
+    case 7:
+        cout << "Sunday";
+        break;
+    default:
+        cout << "Invalid day";
+
+}
+
+}
