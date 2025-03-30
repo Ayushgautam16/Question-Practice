b0f76bf91608054f414750472bf27a021f976c83
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-03-30 21:55:36 +0530

    1

--- main.cpp/basic.java
+++ main.cpp/basic.java
@@ -1,6 +1,27 @@
 public class basic {
 public static void main(String[] args) {
-    
+    // System.out.println("*");
+    // System.out.println("**");
+    // System.out.println("***");
+    // System.out.println("****");
+    // System.out.println("*****");
+    // System.out.println("******");
+
+//  System.out.println("*****");
+//     System.out.println("    *");
+//     System.out.println("   * ");
+//     System.out.println("  *  ");
+//     System.out.println(" *   ");
+//     System.out.println("*****");
+   
+// *****
+//     *
+//    *
+//   *
+//  *
+// *****
+
+
 }
     
 }
