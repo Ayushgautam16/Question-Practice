f732bfb593ed04feca61e6faf20a6caf604f5c59
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-03-30 22:12:35 +0530

    1

--- main.cpp/basic.java
+++ main.cpp/basic.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class basic {
 public static void main(String[] args) {
     // System.out.println("*");
@@ -27,20 +28,31 @@ public static void main(String[] args) {
 
 
 // question 
-int marks  ;
 
-if(marks>= 90){
+System.out.println("enter your marks ");
+Scanner sc = new Scanner(System.in);
+int marks = sc.nextInt();
+
+if(marks> 90){
     System.out.println("excellent");
 }
-else if(marks>= 80){
-    System.out.println("excellent");
+else if(marks> 80){
+    System.out.println("verry goog");
 }
-else if(marks>= 90){
-    System.out.println("excellent");
+else if(marks> 70){
+    System.out.println("good");
 }
-else if(marks>= 90){
-    System.out.println("excellent");
+else if(marks> 60){
+    System.out.println("Average");
 }
+else if(marks> 50){
+    System.out.println("needs improvement");
+}
+else if(marks> 30){
+    System.out.println("below average");
+}
+
+
 
 
 
