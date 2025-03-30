c96b344eb2a4d711223ed222aac358a8d22567e0
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-03-30 22:49:24 +0530

    1

--- main.cpp/basic.java
+++ main.cpp/basic.java
@@ -52,8 +52,63 @@ public static void main(String[] args) {
 //     System.out.println("below average");
 // }
 
+// Scanner sc = new Scanner(System.in);
+// // int n = sc.nextInt();
+// String str = sc.nextLine();
+
+// ************int n = Integer.parseInt(sc.nextLine()); ****************
+// same line me input string or int bhi 
 
 
+
+// // if i wantt both agt same time;
+// for(int i = 0 ; i<=n ; i++){
+//     System.out.println("dear "+ str +" here is the counting "+n );
+
+// }
+
+// i/p 
+
+// ayush5
+// 4
+
+// o/p 
+
+// dear ayush5 here is the counting 4
+// dear ayush5 here is the counting 4
+// dear ayush5 here is the counting 4
+// dear ayush5 here is the counting 4
+// dear ayush5 here is the counting 4
+
+
+
+
+
+// prime or not prime 
+
+
+
+// System.out.println("enter the value you wanted to check: ");
+// Scanner sc = new Scanner(System.in);
+// int n = sc.nextInt();
+
+// int count =0;
+// for (int div =1;div<=n ;div++)
+// {
+//     if(n%div ==0){
+//         count++;
+//     }
+// }
+// if(count==2 ){
+//     System.out.println("prime ");
+// }
+// else{
+//     System.out.println("not prime ");
+// }
+
+// time limit exceeded
+
+// 10to the power 9  operation = 1sec to complete operation 
 
 
 
