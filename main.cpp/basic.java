cef60103110d2dda3ca2c95dccc6151d222b8d09
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-03-30 22:16:50 +0530

    1

--- main.cpp/basic.java
+++ main.cpp/basic.java
@@ -29,28 +29,30 @@ public static void main(String[] args) {
 
 // question 
 
-System.out.println("enter your marks ");
-Scanner sc = new Scanner(System.in);
-int marks = sc.nextInt();
+// System.out.println("enter your marks ");
+// Scanner sc = new Scanner(System.in);
+// int marks = sc.nextInt();
 
-if(marks> 90){
-    System.out.println("excellent");
-}
-else if(marks> 80){
-    System.out.println("verry goog");
-}
-else if(marks> 70){
-    System.out.println("good");
-}
-else if(marks> 60){
-    System.out.println("Average");
-}
-else if(marks> 50){
-    System.out.println("needs improvement");
-}
-else if(marks> 30){
-    System.out.println("below average");
-}
+// if(marks> 90){
+//     System.out.println("excellent");
+// }
+// else if(marks> 80){
+//     System.out.println("verry goog");
+// }
+// else if(marks> 70){
+//     System.out.println("good");
+// }
+// else if(marks> 60){
+//     System.out.println("Average");
+// }
+// else if(marks> 50){
+//     System.out.println("needs improvement");
+// }
+// else if(marks> 30){
+//     System.out.println("below average");
+// }
+
+
 
 
 
